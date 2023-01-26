df63f34549305a56844efb571b44d9ff7e1b9cf7
ben <ben.riegel@mni.thm.de>
2023-01-26 17:32:08 +0100

    i try soooo hard and in the end it did not even matter

--- app/src/main/java/com/aseemwangoo/handsonkotlin/components/navigation/NavGraph.kt
+++ app/src/main/java/com/aseemwangoo/handsonkotlin/components/navigation/NavGraph.kt
@@ -1,7 +1,7 @@
 package com.aseemwangoo.handsonkotlin.components.navigation
 
 import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
+//import androidx.navigation.compose.navigate
 import com.aseemwangoo.handsonkotlin.components.navigation.Destinations.AddTodo
 
 class NavGraph(navController: NavController) {
