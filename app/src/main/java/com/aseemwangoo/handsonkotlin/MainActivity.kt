1b7243cb3866606521b3251aec10c4e4d005a31e
ben <ben.riegel@mni.thm.de>
2023-01-26 16:38:57 +0100

    safe for extrem build changes

--- app/src/main/java/com/aseemwangoo/handsonkotlin/MainActivity.kt
+++ app/src/main/java/com/aseemwangoo/handsonkotlin/MainActivity.kt
@@ -34,8 +34,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           // NavigationComponent()
-            Navigation()
+            NavigationComponent()
+            //Navigation()
         }
 
 //        itemViewModel.isDone.observe(this, { status ->
